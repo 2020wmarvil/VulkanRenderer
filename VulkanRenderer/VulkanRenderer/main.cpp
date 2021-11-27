18ba0488a0d8ae0945a3f6e6e98cc1e83a29f3ab
Wyatt Marvil <2020wmarvil@gmail.com>
2021-11-26 23:54:46 -0500

    Select the physical device

--- VulkanRenderer/VulkanRenderer/main.cpp
+++ VulkanRenderer/VulkanRenderer/main.cpp
@@ -49,6 +49,7 @@ public:
 private:
     GLFWwindow* window;
     VkInstance instance;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
     VkDebugUtilsMessengerEXT debugMessenger;
 
     #pragma region INIT WINDOW
@@ -179,7 +180,30 @@ private:
     
     #pragma region PICK DEVICE
     void pickPhysicalDevice() {
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
 
+        if (deviceCount == 0) {
+            throw std::runtime_error("failed to find GPUs with Vulkan support!");
+        }
+
+        std::vector<VkPhysicalDevice> devices(deviceCount);
+        vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+        for (const auto& device : devices) {
+            if (isDeviceSuitable(device)) {
+                physicalDevice = device;
+                break;
+            }
+        }
+
+        if (physicalDevice == VK_NULL_HANDLE) {
+            throw std::runtime_error("failed to find a suitable GPU!");
+        }
+    }
+
+    bool isDeviceSuitable(VkPhysicalDevice device) {
+        return true;
     }
     #pragma endregion
     
